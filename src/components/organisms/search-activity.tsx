ea087e5675e63fcb4aba31144cdbc717d1902c98
Gabriel Muniz <gabrielmfd@gmail.com>
2025-04-05 17:55:48 -0300

    minor changes

--- src/components/organisms/search-activity.tsx
+++ src/components/organisms/search-activity.tsx
@@ -43,6 +43,7 @@ export default function SearchActivity({ className }: { className?: string }) {
               onClick={() => {
                 setAdventures(filterAdventure ?? []);
                 setSearchedAdventures(debouncedValue);
+                setSearch('');
               }}
             >
               Pesquisar
