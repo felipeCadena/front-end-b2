a964cfba1dc4ce6cc538e5bba8f4cd53535bf08a
b2adventure <adm.b2adventure@gmail.com>
2025-09-08 22:43:12 -0300

    feat: address partner

---
+++ src/utils/getAddress.ts
@@ -0,0 +1,9 @@
+export const getAddress = async (cep: string) => {
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const addressData = await response.json();
+    return addressData;
+  } catch (error) {
+    console.error(error);
+  }
+};
